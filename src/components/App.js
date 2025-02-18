8ecd72f0afc31aa9c7321c9735d3958c24ace2c0
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-02-19 02:19:26 +0300

    search filter still not working

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import Filter from "./Filter";
 
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const {search, setSearch} = useState("");
+
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
+
+  }
+  
+
+  function onSearchChange(event){
+    setSearch(event.target.value);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
       <ShoppingList items={items} />
+      <Filter search={search} onChange={onSearchChange} items={items}/>
     </div>
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Item from "./Item";
 
-function Filter({ onCategoryChange }) {
+
+function Filter({ onCategoryChange, search, onSearchChange ,items}) {
+
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text"
+       name="search" 
+       placeholder="Search..."
+       value={search}
+       onChange={onSearchChange}/>
+
+       <ul> {filteredItems.map((item) =>
+          <li key={item.id}> {item.name} </li> 
+          )}
+        </ul>
+
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
